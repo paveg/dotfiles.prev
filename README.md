371cfd1bb98f70fa7aa8c053986108860f07647e
Ryota Ikezawa <pavegy@gmail.com>
2019-07-31 02:20:19 +0900

    lib and fix

--- README.md
+++ README.md
@@ -1,6 +1,23 @@
 # dotfiles
 
-# Installation
+## Installation
+
+### Prepare
+
+- Install zsh
+
+    - macOS
+    ```bash
+    $ /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    $ brew install zsh
+    ```
+
+    - Linux
+    ```bash
+    $ sudo apt-get update && sudo apt-get install -y zsh
+    ```
+
+### Actual
 
 ```bash
 $ curl -L raw.github.com/paveg/dotfiles/master/install.sh | bash
--- install.sh
+++ install.sh
@@ -1,23 +1,15 @@
 #!/bin/bash
-set -ex
+set -e
+export PLATFORM
+source ./lib/utilities.sh
 
-install_zsh() {
-    if [ "$(uname)" = "Darwin" ]; then
-        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        brew install zsh
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        sudo apt-get update && sudo apt-get install -y zsh
-    fi
-}
-
-apply_zsh() {
-    sudo bash -c "echo '/usr/local/bin/zsh' >> /etc/shells"
-    chsh -s $(which zsh)
-}
-
-echo "[INFO] starting the installation..."
-
-install_zsh
-apply_zsh
-
-echo "[INFO] finish installing, ok."
+log_pass "Starting the installation..."
+# Set DOTPATH as default variable
+if [ -z "${DOTPATH:-}" ]; then
+    DOTPATH=~/.dotfiles; export DOTPATH
+fi
+if [ -d "$DOTPATH" ]; then
+    log_fail "$DOTPATH: already exists"
+    exit 1
+fi
+log_pass "dotfiles ok."
