96e95033be14fbe913d1cef22a0c88f7df4cb4fc
Ryota Ikezawa <pavegy@gmail.com>
2019-07-31 01:13:40 +0900

    Update

--- install.sh
+++ install.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
+set -ex
+
+install_zsh() {
+    if [ "$(uname)" = "Darwin" ]; then
+        /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+        brew install zsh
+    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+        sudo apt-get update && sudo apt-get install -y zsh
+    fi
+}
+
+apply_zsh() {
+    sudo bash -c "echo '/usr/local/bin/zsh' >> /etc/shells"
+    chsh -s $(which zsh)
+}
 
 echo "[INFO] starting the installation..."
 
-if [ "$(uname)" = "Darwin" ]; then
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  brew install zsh
-elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-  apt install zsh
-fi
+install_zsh
+apply_zsh
 
 echo "[INFO] finish installing, ok."
