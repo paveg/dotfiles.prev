2272156fb01908d9b9ae085f81699be2acd85393
Ryota Ikezawa <pavegy@gmail.com>
2019-07-31 02:45:38 +0900

    Install homebrew

--- install.sh
+++ install.sh
@@ -3,7 +3,23 @@ set -e
 export PLATFORM
 source ./lib/utilities.sh
 
+# install Homebrew
+prepare() {
+    if has "brew"; then
+        log_info "already installed Homebrew"
+        return
+    fi
+    if is_osx; then
+       /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    elif is_linux; then
+        sudo apt-get update && sudo apt-get install build-essential curl file git
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+    fi
+}
+
 log_pass "Starting the installation..."
+prepare
+
 # Set DOTPATH as default variable
 if [ -z "${DOTPATH:-}" ]; then
     DOTPATH=~/.dotfiles; export DOTPATH
--- lib/utilities.sh
+++ lib/utilities.sh
@@ -172,3 +172,14 @@ is_debug() {
         return 1
     fi
 }
+
+# is_exists returns true if executable $1 exists in $PATH
+is_exists() {
+    which "$1" >/dev/null 2>&1
+    return $?
+}
+
+# has is wrapper function
+has() {
+    is_exists "$@"
+}
