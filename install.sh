211607681aae7cc6de2626473b0ce068b510a5f5
Ryota Ikezawa <ikezawa-ryota@c-fo.com>
2019-07-31 13:29:00 +0900

    update

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ prepare() {
     if is_osx; then
         /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
     elif is_linux; then
-        sudo apt-get update && sudo apt-get install -y --no-install-recommends build-essential curl file git gcc
+        sudo apt-get update && sudo apt-get install -y --no-install-recommends build-essential ca-certificates curl file git gcc
         sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
         echo 'eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)' >> ~/.profile
         source ~/.profile
