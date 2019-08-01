dabbab565cf0fdf06f82eb3353404fda6afc5819
Ryota Ikezawa <ikezawa-ryota@c-fo.com>
2019-08-01 21:14:13 +0900

    Extract package list

---
+++ lib/package_list.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -u
+
+declare -ax FOMURAS=(docker ghq tree direnv anyenv wget htop tmux fzf tig jq)
+declare -ax CASK_PACKAGES=( alfred clipy keycastr shiftit spotify google-japanese-ime docker karabiner-elements )
+
