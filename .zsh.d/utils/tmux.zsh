872e6f4b652e76f4e55aa6e441a82298f08b4930
Ryota Ikezawa <pavegy@gmail.com>
2019-08-04 12:38:49 +0900

    Update for tmux

--- .zsh.d/utils/tmux.zsh
+++ .zsh.d/utils/tmux.zsh
@@ -8,14 +8,14 @@ tmux_automatically_attach_session() {
   if is_screen_or_tmux_running; then
     ! is_exists 'tmux' && return 1
     if is_tmux_running; then
-      echo "tmux starting...."
+      log_info "Start to tmux session..."
     elif is_screen_running; then
-      echo "This is on screen."
+      log_info "This is on screen."
     fi
   else
     if shell_has_started_interactively && ! is_ssh_running; then
       if ! is_exists 'tmux'; then
-        echo 'Error: tmux command not found' 2>&1
+        log_fail 'Error: tmux command not found' 2>&1
         return 1
       fi
 
@@ -27,13 +27,13 @@ tmux_automatically_attach_session() {
         if [[ "$REPLY" =~ ^[Yy]$ ]] || [[ "$REPLY" == '' ]]; then
           tmux attach-session
           if [ $? -eq 0 ]; then
-            echo "$(tmux -V) attached session"
+            log_info "$(tmux -V) attached session"
             return 0
           fi
         elif [[ "$REPLY" =~ ^[0-9]+$ ]]; then
           tmux attach -t "$REPLY"
           if [ $? -eq 0 ]; then
-            echo "$(tmux -V) attached session"
+            log_info "$(tmux -V) attached session"
             return 0
           fi
         fi
@@ -43,9 +43,9 @@ tmux_automatically_attach_session() {
         # on OS X force tmux's default command
         # to spawn a shell in the user's namespace
         tmux_config=$(cat $HOME/.tmux.conf <(echo 'set-option -g default-command "reattach-to-user-namespace -l $SHELL"'))
-        tmux -f <(echo "$tmux_config") new-session && echo "$(tmux -V) created new session supported OS X"
+        tmux -f <(echo "$tmux_config") new-session && log_info "$(tmux -V) created new session supported OS X"
       else
-        tmux new-session && echo "tmux created new session"
+        tmux new-session && log_info "tmux created new session"
       fi
     fi
   fi
