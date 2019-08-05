7b544c2087840958eb3719738c3cb3857bc49451
Ryota Ikezawa <pavegy@gmail.com>
2019-08-06 01:52:23 +0900

    ink

--- etc/install.sh
+++ etc/install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -eu
 
-# functions
+# utility functions ------------------------------
+
 isExists() {
   command -v "$1" >/dev/null 2>&1
   return $?
@@ -15,10 +16,107 @@ isNotExist() {
   fi
 }
 
+ink() {
+  if [[ "$#" -eq 0 ]] || [[ "$#" -gt 2 ]]; then
+    echo "Usage: ink <color> <text>"
+    echo "Colors:"
+    echo "  black, white, red, green, yellow, blue, purple, cyan, gray"
+    return 1
+  fi
+
+  local open="\033["
+  local close="${open}0m"
+  local black="0;30m"
+  local red="1;31m"
+  local green="1;32m"
+  local yellow="1;33m"
+  local blue="1;34m"
+  local purple="1;35m"
+  local cyan="1;36m"
+  local gray="0;37m"
+  local white="$close"
+
+  local text="$1"
+  local color="$close"
+
+  if [[ "$#" -eq 2 ]]; then
+    text="$2"
+    case "$1" in (black | red | green | yellow | blue | purple | cyan | gray | white)
+      eval color="\$$1"
+      ;;
+    esac
+  fi
+
+  printf "${open}${color}${text}${close}"
+}
+
+logging() {
+  if [[ "$#" -eq 0 ]] || [[ "$#" -gt 2 ]]; then
+    echo "Usage: ink <fmt> <msg>"
+    echo "Formatting Options:"
+    echo "  TITLE, ERROR, WARN, INFO, SUCCESS"
+    return 1
+  fi
+
+  local color=
+  local text="$2"
+
+  case "$1" in
+  TITLE)
+    color=yellow
+    ;;
+  ERROR | WARN)
+    color=red
+    ;;
+  INFO)
+    color=blue
+    ;;
+  SUCCESS)
+    color=green
+    ;;
+  *)
+    text="$1"
+    ;;
+  esac
+
+  timestamp() {
+    ink gray "["
+    ink purple "$(date +%H:%M:%S)"
+    ink gray "] "
+  }
+
+  timestamp
+  ink "$color" "$text"
+  echo
+}
+
+log_pass() {
+  logging SUCCESS "$1"
+}
+
+log_fail() {
+  logging ERROR "$1" 1>&2
+}
+
+log_warn() {
+  logging WARN "$1"
+}
+
+log_info() {
+  logging INFO "$1"
+}
+
+log_echo() {
+  logging TITLE "$1"
+}
+
+
+# -----------------------------------------
+
 # TODO: tool check
 # git
 
-echo "start installing..."
+log_info "start installing..."
 
 readonly githubUser="paveg"
 readonly githubRepo="dotfiles"
@@ -42,31 +140,34 @@ fi
 if isNotExist brew; then
   # for macOS
   ruby -e "$(curl -fsSL https://githubusercontent.com/Homebrew/install/master/install)"
+  log_pass "Homebrew installation is completed."
 else
-  echo "Homebrew is already installed."
+  log_warn "Homebrew is already installed."
 fi
 
-echo "Install Applications..."
+log_info "Install Applications..."
 while read pkg
 do
-  if ! brew cask list | grep "$pkg" &>/dev/null; then
-    echo "install $pkg..."
-    brew cask install "$pkg"
+  if ! brew cask list | grep $pkg &>/dev/null; then
+    log_info "Installing $pkg..."
+    brew cask install $pkg
   else
-    echo "$pkg is already installed."
+    log_warn "$pkg is already installed."
   fi
 done < "$repoDir/pkg/brew_cask.txt"
+log_pass "Applications installation is completed."
 
-echo "Install Packages..."
+log_info "Install Packages..."
 while read pkg
 do
-  if ! brew list | grep "$pkg" &>/dev/null; then
-    echo "install $pkg"
-    brew install "$pkg"
+  if ! brew list | grep $pkg &>/dev/null; then
+    log_info "Installing $pkg..."
+    brew install $pkg
   else
-    echo "$pkg is already installed"
+    log_warn "$pkg is already installed"
   fi
 done < "$repoDir/pkg/brew.txt"
+log_pass "Packages installation is completed."
 
-echo "dotfiles OK."
+log_pass "dotfiles OK."
 
